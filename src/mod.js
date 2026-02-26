8cfa31af63c4673010e41f9e092819d9b43af191
Jesper van den Ende <jespertheend@gmail.com>
2026-02-26 13:13:48 +0100

    fix: Make some props aren't mangled

--- src/mod.js
+++ src/mod.js
@@ -1,6 +1,24 @@
 import { TypedMessenger } from "../deps/vendor/raw.githubusercontent.com/rendajs/Renda/2e6b39958c7db0e2b9eefb861fbc7ede23104bce/src/util/TypedMessenger/TypedMessenger.js";
 import { TimeoutError } from "../deps/vendor/raw.githubusercontent.com/rendajs/Renda/2e6b39958c7db0e2b9eefb861fbc7ede23104bce/src/util/TimeoutError.js";
 
+const props = /** @type {const} */ ({
+	adLadIframeBridgeMessage: "adLadIframeBridgeMessage",
+	setNeedsMute: "setNeedsMute",
+	setNeedsPause: "setNeedsPause",
+	init: "init",
+	showFullScreenAd: "showFullScreenAd",
+});
+
+// @ts-ignore We want to make sure that `props` remains an object.
+// Normally, terser would turn every property into a separate variable.
+// This would be fine for the first pass of minification, but if a user
+// were to bundle and minify this libarry with their own code, they will minify a second time
+// causing all these props to lose their quotes.
+// This can be an issue if the new bundle has property mangling enabled.
+// This if statement will never run, but rollup and terser will both think it
+// might and so the `props` opbject will remain an object.
+if (props > 0) console.log(props);
+
 export function iframeBridgePlugin() {
 	const messenger = new TypedMessenger();
 
@@ -16,31 +34,31 @@ export function iframeBridgePlugin() {
 			}
 
 			messenger.setSendHandler((data) => {
-				window.parent.postMessage({ adLadIframeBridgeMessage: data.sendData }, "*", data.transfer);
+				window.parent.postMessage({ [props.adLadIframeBridgeMessage]: data.sendData }, "*", data.transfer);
 			});
 			globalThis.addEventListener("message", (event) => {
 				if (typeof event.data == "object" && event.data && "adLadIframeBridgeMessage" in event.data) {
-					messenger.handleReceivedMessage(event.data.adLadIframeBridgeMessage);
+					messenger.handleReceivedMessage(event.data[props.adLadIframeBridgeMessage]);
 				}
 			});
 			messenger.setResponseHandlers({
 				/**
 				 * @param {boolean} needsMute
 				 */
-				setNeedsMute: (needsMute) => {
+				[props.setNeedsMute]: (needsMute) => {
 					ctx.setNeedsMute(needsMute);
 				},
 				/**
 				 * @param {boolean} needsPause
 				 */
-				setNeedsPause: (needsPause) => {
+				[props.setNeedsPause]: (needsPause) => {
 					ctx.setNeedsPause(needsPause);
 				},
 			});
 
 			let initResult;
 			try {
-				initResult = await messenger.sendWithOptions.init({
+				initResult = await messenger.sendWithOptions[props.init]({
 					timeout: 10_000,
 				});
 			} catch (error) {
@@ -60,7 +78,7 @@ export function iframeBridgePlugin() {
 			}
 		},
 		async showFullScreenAd() {
-			return await messenger.send.showFullScreenAd();
+			return await messenger.send[props.showFullScreenAd]();
 		},
 	});
 
